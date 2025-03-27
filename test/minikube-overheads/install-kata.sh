388d5dde6334038e218a54288519f95b82b79149
Michele Beretta <beretta.m98@gmail.com>
2025-03-27 15:44:05 +0100

    Move kata-containers submodule, add minikube setup

--- test/minikube-overheads/install-kata.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd kata-containers/tools/packaging/kata-deploy
-
-kubectl apply -f kata-rbac/base/kata-rbac.yaml
-kubectl apply -f kata-deploy/base/kata-deploy.yaml
-
-kubectl apply -f runtimeclasses/kata-runtimeClasses.yaml
