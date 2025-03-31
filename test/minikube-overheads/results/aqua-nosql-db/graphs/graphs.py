74658c8b75be30f9f633a99a09bd93095b923de5
Michele Beretta <beretta.m98@gmail.com>
2025-03-31 18:19:10 +0200

    Update graphs for NoSQL DBs

--- test/minikube-overheads/results/aqua-nosql-db/graphs/graphs.py
+++ test/minikube-overheads/results/aqua-nosql-db/graphs/graphs.py
@@ -12,64 +12,84 @@ data = {
         'containerd': {
             'read':  {'P90': 331, 'P99': 419},
             'write': {'P90': 314, 'P99': 429},
+            'throughput': 2645.50,
         },
         'gVisor':  {
             'read':  {'P90': 435, 'P99': 554},
             'write': {'P90': 408, 'P99': 500},
+            'throughput': 2183.41,
         },
         'QEMU':    {
             'read':  {'P90': 401, 'P99': 536},
             'write': {'P90': 371, 'P99': 448},
+            'throughput': 2398.08,
         },
     },
     'Redis': {
         'containerd': {
             'read':  {'P90':  68, 'P99': 111},
             'write': {'P90':  75, 'P99':  98},
+            'throughput': 16949.15,
         },
         'gVisor':  {
             'read':  {'P90': 140, 'P99': 198},
             'write': {'P90': 150, 'P99': 189},
+            'throughput': 8547.01,
         },
         'QEMU':    {
             'read':  {'P90': 100, 'P99': 124},
             'write': {'P90': 109, 'P99': 147},
+            'throughput': 10989.01,
         },
     },
 }
 
-# ------------------------------------ Latency graphs
+width = 0.25    # bar width
+groups = data.keys()
+xs = np.arange(len(groups))
+latencies = {}
+throughputs = {}
 
-def mean(*args):
-    return sum(args) / len(args)
+OUTSIDE_LEGEND_PARAMS = {
+    'bbox_to_anchor': (-0.06, 1, 1, 0.2), # x0, y0, width, height
+    'frameon': False,
+    'loc': 'lower left',
+    'ncol': 3,
+    'handlelength': 1,
+}
 
-for dbms in data.keys():
-    width = 0.25   # bar width
+for method in ['containerd', 'gVisor', 'QEMU']:
+    # ------------------------------------ Latency
 
-    groups = list(data[dbms].keys())
-    percentiles = {}
-    for perc in ['P90', 'P99']:
-        percentiles[perc] = [mean(data[dbms][g]['read'][perc], data[dbms][g]['write'][perc]) for g in groups]
-        
+    latencies[method] = [max(data[g][method]['read']['P99'], data[g][method]['write']['P99']) for g in groups]
+
     plt.figure()
-
-    x = np.arange(len(groups))
     m = 0
-    for attribute, measurement in percentiles.items():
+    for label, values in latencies.items():
         offset = width * m
-        plt.bar(x + offset, measurement, width, label=attribute)
+        plt.bar(xs + offset, values, width=width, label=label)
         m += 1
 
-    plt.xticks(x + width / 2, groups)
-    plt.ylabel('Latency [μs]')
+    plt.xticks(xs + width, groups)
+    plt.ylabel('Latency [ms]')
+    plt.legend()
+    plt.savefig('latency.pdf', bbox_inches='tight')
+
+    # ------------------------------------ Throughput
+
+    throughputs[method] = [data[g][method]['throughput'] / 1000 for g in groups]
+
+    plt.figure()
+    m = 0
+    for label, values in throughputs.items():
+        offset = width * m
+        plt.bar(xs + offset, values, width=width, label=label)
+        m += 1
+
+    plt.xticks(xs + width, groups)
+    yticks = range(0, 21, 5)
+    plt.yticks(ticks=yticks, labels=[f'{k}K' if k != 0 else '0' for k in yticks])
+    plt.ylabel('Throughput [req/s]')
+    plt.ylim(0, 21)
     plt.legend(loc='upper left')
-    plt.ylim(0, 565)
-
-    plt.savefig(f'{dbms}-latency.pdf', bbox_inches='tight')
-
-    print('For DBMS', dbms)
-    for p in ['P90', 'P99']:
-        print(f'  {p}')
-        for g in ['gVisor', 'QEMU']:
-            inc = mean(data[dbms][g]['read'][p], data[dbms][g]['write'][p]) / mean(data[dbms]['containerd']['read'][perc], data[dbms]['containerd']['write'][p]) - 1
-            print(f'    {g}:', inc * 100)
+    plt.savefig('throughput.pdf', bbox_inches='tight')
