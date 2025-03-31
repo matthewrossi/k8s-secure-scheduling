5a8eaf3eb6697a0361cac0fb9868cffae25a8dc6
Michele Beretta <beretta.m98@gmail.com>
2025-03-31 14:24:42 +0200

    Add warmup time to Wrk test

--- test/minikube-overheads/run-test-web-servers.sh
+++ test/minikube-overheads/run-test-web-servers.sh
@@ -12,5 +12,8 @@ for service in $(kubectl get services -l type=web-server -o name); do
   port=$(kubectl get $service -o json | jq '.spec.ports[0].nodePort')
   echo "[*] Testing $service at $(minikube ip):$port"
   name=$(echo $service | cut -d'/' -f2)
+  # Warmup
+  wrk -t $WRK_THREADS -c $WRK_CONNS -d 30s --latency "http://$(minikube ip):$port" > /dev/null
+  # Actual test
   wrk -t $WRK_THREADS -c $WRK_CONNS -d $WRK_TIME --latency "http://$(minikube ip):$port" > $out/$name
 done
