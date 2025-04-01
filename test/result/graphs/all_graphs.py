bc3cb47e94b43b3c6eacc790a62314d5c15a11e1
Michele Beretta <beretta.m98@gmail.com>
2025-04-01 10:30:45 +0200

    Add code for scheduling graphs

--- test/result/graphs/all_graphs.py
+++ test/result/graphs/all_graphs.py
@@ -7,144 +7,92 @@ from pathlib import Path
 
 # For LaTeX-like fonts, comment if it errors out
 plt.rcParams['font.family'] = 'NewComputerModern08'
-plt.rcParams['font.size'] = 15
+plt.rcParams['font.size'] = 18
 
+BAR_WIDTH = 0.25
 OUTSIDE_LEGEND_PARAMS = {
     'bbox_to_anchor': (0, 1.02, 1, 0.2), # x0, y0, width, height
     'frameon': False,
+    'handlelength': 1,
     'loc': 'lower left',
-    'ncol': 3,
+    'ncol': 2,
 }
 
-FOLDER = Path('../aqua-3cn-1000n-30kpods-uniform')
+# baseline taken from ../aqua-baseline-3cn-1000n-30kpods-uniform, from vanilla tests
+# other data taken from ../aqua-3cn-1000n-30kpods-uniform
+#
+# Scheduling Latency in ms (if more values of p99, then the max)
+# Throughput is qps (max value, if given more values then the average of the max excluding obvious outliers)
 
-def get_type(path, types=[]):
-    for t in types:
-        if t in path.as_posix():
-            return t
-    return None
+data = {
+    'DS': {
+        'scheduling-latency-p99': {
+            'Baseline': 1.70,
+            'Unconstrained': 1.80,
+            'Constrained': 3.83,
+        },
+        'scheduling-throughput': {
+            'Baseline': 146,
+            'Unconstrained': 140,
+            'Constrained': 137,
+        },
+    },
+    'MT': {
+        'scheduling-latency-p99': {
+            'Baseline': 1.68,
+            'Unconstrained': 1.82,
+            'Constrained': 3.69,
+        },
+        'scheduling-throughput': {
+            'Baseline': 143,
+            'Unconstrained': 140,
+            'Constrained': 58.6,
+        },
+    },
+    'ISL': {
+        'scheduling-latency-p99': {
+            'Baseline': 1.75,
+            'Unconstrained': 1.87,
+            'Constrained': 3.51,
+        },
+        'scheduling-throughput': {
+            'Baseline': 179,
+            'Unconstrained': 143,
+            'Constrained': 176,
+        },
+    },
+}
 
+groups = data.keys()
+xs = np.arange(len(groups))
+latencies = {}
+throughputs = {}
 
-def grouped_bars(use_case, files, types, labels, value_transformers, bar_width=0.25, xticks_offset=0.25, ylabel='', ylim=None, output=''):
-    data = {}
-    for key, _ in value_transformers:
-        data[key] = {}
-
-    for path in (FOLDER / use_case).glob(files):
-        test_type = get_type(path, types)
-        with open(path) as f:
-            values = json.load(f)
-
-        for key, f in value_transformers:
-            data[key][test_type] = f(values)
-
-    x = np.arange(len(types))
-
+for method in ['Baseline', 'Unconstrained', 'Constrained']:
+    latencies[method] = [data[g]['scheduling-latency-p99'][method] for g in groups]
     plt.figure()
-    for i, (attribute, measurements) in enumerate(data.items()):
-        offset = bar_width * i
-        values = [measurements[t] for t in types]
-        plt.bar(x + offset, values, bar_width, label=attribute)
+    m = 0
+    for label, values in latencies.items():
+        offset = BAR_WIDTH * m
+        plt.bar(xs + offset, values, width=BAR_WIDTH, label=label)
+        m += 1
 
-    plt.ylabel(ylabel)
-    plt.xticks(x + xticks_offset, [labels[t] for t in types])
-    # plt.legend(**OUTSIDE_LEGEND_PARAMS)
-    plt.legend(loc='upper left')
-    plt.ylim(ylim)
-    plt.savefig(f'out/{output}.pdf', bbox_inches='tight')
+    plt.xticks(xs + BAR_WIDTH, groups)
+    plt.ylabel('Latency [ms]')
+    plt.legend(**OUTSIDE_LEGEND_PARAMS)
+    plt.savefig('scheduling-latency.pdf', bbox_inches='tight')
 
+    throughputs[method] = [data[g]['scheduling-throughput'][method] for g in groups]
+    plt.figure()
+    m = 0
+    for label, values in throughputs.items():
+        offset = BAR_WIDTH * m
+        plt.bar(xs + offset, values, width=BAR_WIDTH, label=label)
+        m += 1
 
-def scheduling_throughput(use_case, types, labels):
-    grouped_bars(use_case,
-                 files='SchedulingThroughputPrometheus*.json',
-                 types=types,
-                 labels=labels,
-                 value_transformers=[
-                     ('Average', lambda x: x['avg']),
-                     ('Maximum', lambda x: x['max']),
-                 ],
-                 xticks_offset=0.125,
-                 ylabel='Throughput [qps]',
-                 ylim=(0, 75),
-                 output=f'{use_case}-scheduling-throughput')
-
-
-def scheduling_latency(use_case, types, labels):
-    grouped_bars(use_case,
-                 files='SchedulingMetrics*.json',
-                 types=types,
-                 labels=labels,
-                 value_transformers=[
-                     ('P50', lambda x: x['schedulingLatency']['Perc50'] / 1e6),
-                     ('P90', lambda x: x['schedulingLatency']['Perc90'] / 1e6),
-                     ('P99', lambda x: x['schedulingLatency']['Perc99'] / 1e6),
-                 ],
-                 ylabel='Latency [ms]',
-                 ylim=(0, 5),
-                 output=f'{use_case}-scheduling-latency')
-
-
-def prometheus_mutation_duration(use_case, types, labels):
-    grouped_bars(use_case,
-                 files='GenericPrometheusQuery GatekeeperMutationRequestDuration*.json',
-                 types=types,
-                 labels=labels,
-                 value_transformers=[
-                     ('P50', lambda x: x['dataItems'][0]['data']['Perc50'] * 1000),
-                     ('P90', lambda x: x['dataItems'][0]['data']['Perc90'] * 1000),
-                     ('P99', lambda x: x['dataItems'][0]['data']['Perc99'] * 1000),
-                 ],
-                 ylabel='Duration [ms]',
-                 ylim=(0, 1.3),
-                 output=f'{use_case}-prometheus-mutation-duration')
-
-
-def prometheus_validation_duration(use_case, types, labels):
-    grouped_bars(use_case,
-                 files='GenericPrometheusQuery GatekeeperValidationRequestDuration*.json',
-                 types=types,
-                 labels=labels,
-                 value_transformers=[
-                     ('P50', lambda x: x['dataItems'][0]['data']['Perc50'] * 1000),
-                     ('P90', lambda x: x['dataItems'][0]['data']['Perc90'] * 1000),
-                     ('P99', lambda x: x['dataItems'][0]['data']['Perc99'] * 1000),
-                 ],
-                 ylabel='Duration [ms]',
-                 ylim=(0, 1.3),
-                 output=f'{use_case}-prometheus-validation-duration')
-
-# ================================================ Multi tenancy
-
-# ------------------------- Scheduling Throughput
-
-TYPES = ['vanilla', 'uc1', 'uc2']
-LABELS = {'uc1': 'Tenant A', 'uc2': 'Tenant B', 'vanilla': 'Unconstrained'}
-
-scheduling_throughput(use_case='multi-tenancy-test', types=TYPES, labels=LABELS)
-scheduling_latency(use_case='multi-tenancy-test', types=TYPES, labels=LABELS)
-prometheus_mutation_duration(use_case='multi-tenancy-test', types=TYPES, labels=LABELS)
-prometheus_validation_duration(use_case='multi-tenancy-test', types=TYPES, labels=LABELS)
- 
-# ================================================ Data Sovereignty
-
-# ------------------------- Scheduling Throughput
-
-TYPES = ['vanilla', 'eu-region', 'us-region']
-LABELS = {'eu-region': 'EEA', 'us-region': 'US', 'vanilla': 'Unconstrained'}
-
-scheduling_throughput(use_case='data-sovereignty-test', types=TYPES, labels=LABELS)
-scheduling_latency(use_case='data-sovereignty-test', types=TYPES, labels=LABELS)
-prometheus_mutation_duration(use_case='data-sovereignty-test', types=TYPES, labels=LABELS)
-prometheus_validation_duration(use_case='data-sovereignty-test', types=TYPES, labels=LABELS)
-
-# ================================================ Workload Security Rings
-
-# ------------------------- Scheduling Throughput
-
-TYPES = ['vanilla', 'unhandened', 'sensitive'] # yes, there's a typo in the file names
-LABELS = {'unhandened': 'Unhardened', 'sensitive': 'Sensitive', 'vanilla': 'Unconstrained'}
-
-scheduling_throughput(use_case='workload-security-rings-test', types=TYPES, labels=LABELS)
-scheduling_latency(use_case='workload-security-rings-test', types=TYPES, labels=LABELS)
-prometheus_mutation_duration(use_case='workload-security-rings-test', types=TYPES, labels=LABELS)
-prometheus_validation_duration(use_case='workload-security-rings-test', types=TYPES, labels=LABELS)
+    plt.xticks(xs + BAR_WIDTH, groups)
+    # yticks = range(0, 200, 40)
+    # plt.yticks(ticks=yticks, labels=[f'{k}K' if k != 0 else '0' for k in yticks])
+    plt.ylabel('Throughput [qps]')
+    plt.legend(**OUTSIDE_LEGEND_PARAMS)
+    plt.savefig('scheduling-throughput.pdf', bbox_inches='tight')
